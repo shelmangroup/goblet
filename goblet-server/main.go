2d465e3c6d861dce041dae40af985afdc0389e96
Lars Sjöström <lars@radicore.se>
2020-12-15 19:40:51 +0100

    more clean up

--- goblet-server/main.go
+++ goblet-server/main.go
@@ -15,7 +15,6 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"io"
@@ -23,37 +22,17 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"os"
 	"time"
 
-	"cloud.google.com/go/errorreporting"
-	"cloud.google.com/go/logging"
-	"cloud.google.com/go/storage"
-	"contrib.go.opencensus.io/exporter/stackdriver"
 	"github.com/google/goblet"
 	googlehook "github.com/google/goblet/google"
-	"github.com/google/uuid"
 	"go.opencensus.io/stats/view"
 	"go.opencensus.io/tag"
-	//	"golang.org/x/oauth2/google"
-
-	logpb "google.golang.org/genproto/googleapis/logging/v2"
-)
-
-const (
-	scopeCloudPlatform = "https://www.googleapis.com/auth/cloud-platform"
-	scopeUserInfoEmail = "https://www.googleapis.com/auth/userinfo.email"
 )
 
 var (
 	port      = flag.Int("port", 8080, "port to listen to")
 	cacheRoot = flag.String("cache_root", "", "Root directory of cached repositories")
-
-	stackdriverProject      = flag.String("stackdriver_project", "", "GCP project ID used for the Stackdriver integration")
-	stackdriverLoggingLogID = flag.String("stackdriver_logging_log_id", "", "Stackdriver logging Log ID")
-
-	backupBucketName   = flag.String("backup_bucket_name", "", "Name of the GCS bucket for backed-up repositories")
-	backupManifestName = flag.String("backup_manifest_name", "", "Name of the backup manifest")
 
 	latencyDistributionAggregation = view.Distribution(
 		100,
@@ -134,89 +113,6 @@ func main() {
 		log.Printf("Starting %s for %s", action, u.String())
 		return &logBasedOperation{action, u}
 	}
-	var backupLogger *log.Logger = log.New(os.Stderr, "", log.LstdFlags)
-	if *stackdriverProject != "" {
-		// Error reporter
-		ec, err := errorreporting.NewClient(context.Background(), *stackdriverProject, errorreporting.Config{
-			ServiceName: "goblet",
-		})
-		if err != nil {
-			log.Fatalf("Cannot create a Stackdriver errorreporting client: %v", err)
-		}
-		defer func() {
-			if err := ec.Close(); err != nil {
-				log.Printf("Failed to report errors to Stackdriver: %v", err)
-			}
-		}()
-		er = func(r *http.Request, err error) {
-			ec.Report(errorreporting.Entry{
-				Req:   r,
-				Error: err,
-			})
-			log.Printf("Error while processing a request: %v", err)
-		}
-
-		if *stackdriverLoggingLogID != "" {
-			lc, err := logging.NewClient(context.Background(), *stackdriverProject)
-			if err != nil {
-				log.Fatalf("Cannot create a Stackdriver logging client: %v", err)
-			}
-			defer func() {
-				if err := lc.Close(); err != nil {
-					log.Printf("Failed to log requests to Stackdriver: %v", err)
-				}
-			}()
-
-			// Request logger
-			sdLogger := lc.Logger(*stackdriverLoggingLogID)
-			rl = func(r *http.Request, status int, requestSize, responseSize int64, latency time.Duration) {
-				sdLogger.Log(logging.Entry{
-					HTTPRequest: &logging.HTTPRequest{
-						Request:      r,
-						RequestSize:  requestSize,
-						Status:       status,
-						ResponseSize: responseSize,
-						Latency:      latency,
-						RemoteIP:     r.RemoteAddr,
-					},
-				})
-			}
-			lrol = func(action string, u *url.URL) goblet.RunningOperation {
-				op := &stackdriverBasedOperation{
-					sdLogger:  sdLogger,
-					action:    action,
-					u:         u,
-					startTime: time.Now(),
-					id:        uuid.New().String(),
-				}
-				op.sdLogger.Log(logging.Entry{
-					Payload: &LongRunningOperation{
-						Action: op.action,
-						URL:    op.u.String(),
-					},
-					Operation: &logpb.LogEntryOperation{
-						Id:       op.id,
-						Producer: "github.com/google/goblet",
-						First:    true,
-					},
-				})
-				return op
-			}
-			// Backup logger
-			backupLogger = sdLogger.StandardLogger(logging.Warning)
-		}
-
-		// OpenCensus view exporters.
-		exporter, err := stackdriver.NewExporter(stackdriver.Options{
-			ProjectID: *stackdriverProject,
-		})
-		if err != nil {
-			log.Fatal(err)
-		}
-		if err = exporter.StartMetricsExporter(); err != nil {
-			log.Fatal(err)
-		}
-	}
 
 	config := &goblet.ServerConfig{
 		LocalDiskCacheRoot:         *cacheRoot,
@@ -224,15 +120,6 @@ func main() {
 		ErrorReporter:              er,
 		RequestLogger:              rl,
 		LongRunningOperationLogger: lrol,
-	}
-
-	if *backupBucketName != "" && *backupManifestName != "" {
-		gsClient, err := storage.NewClient(context.Background())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		googlehook.RunBackupProcess(config, gsClient.Bucket(*backupBucketName), *backupManifestName, backupLogger)
 	}
 
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
@@ -262,46 +149,4 @@ func (op *logBasedOperation) Printf(format string, a ...interface{}) {
 
 func (op *logBasedOperation) Done(err error) {
 	log.Printf("Finished %s for %s: %v", op.action, op.u.String(), err)
-}
-
-type stackdriverBasedOperation struct {
-	sdLogger  *logging.Logger
-	action    string
-	u         *url.URL
-	startTime time.Time
-	id        string
-}
-
-func (op *stackdriverBasedOperation) Printf(format string, a ...interface{}) {
-	lro := &LongRunningOperation{
-		Action:          op.action,
-		URL:             op.u.String(),
-		ProgressMessage: fmt.Sprintf(format, a...),
-	}
-	op.sdLogger.Log(logging.Entry{
-		Payload: lro,
-		Operation: &logpb.LogEntryOperation{
-			Id:       op.id,
-			Producer: "github.com/google/goblet",
-		},
-	})
-}
-
-func (op *stackdriverBasedOperation) Done(err error) {
-	lro := &LongRunningOperation{
-		Action:     op.action,
-		URL:        op.u.String(),
-		DurationMs: int(time.Since(op.startTime) / time.Millisecond),
-	}
-	if err != nil {
-		lro.Error = err.Error()
-	}
-	op.sdLogger.Log(logging.Entry{
-		Payload: lro,
-		Operation: &logpb.LogEntryOperation{
-			Id:       op.id,
-			Producer: "github.com/google/goblet",
-			Last:     true,
-		},
-	})
 }
